ea4699fd77faa5258ada00a4d29374a33cbeb5c6
Alexandre Rademaker <arademaker@gmail.com>
2021-03-02 16:22:02 +0000

    Chapter2.hs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # demo
+
+Repositório de cóódigos do curso de `Análise de Algoritmos` da EMAp/FGV em 2021.1
+
---
+++ src/Chapter2.hs
@@ -0,0 +1,7 @@
+module Chapter2 where
+
+-- Exercio A: “Is a half of two plus two equal to two or three?”
+
+let answerA = (2+2 `div` 2) in y == 2 || y == 3
+
+-- Tarefa, completar este modulo cada aluno contribuindo com a solucao de um exercicio do capitulo 2 do livro Thinking Functionally with Haskell (TFwH).
