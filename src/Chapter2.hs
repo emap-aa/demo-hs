0a79ff55c9d0020c34b8a4b12c130f39833029d5
Alexandre Rademaker <arademaker@gmail.com>
2021-03-02 13:35:35 -0300

    correcao codigo Chapter2.hs

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -1,7 +1,13 @@
+
 module Chapter2 where
 
 -- Exercio A: “Is a half of two plus two equal to two or three?”
 
-let answerA = (2+2 `div` 2) in y == 2 || y == 3
+ansA = x == 2 || x == 3
+  where
+    x = (2 + 2) `div` 2 
 
--- Tarefa, completar este modulo cada aluno contribuindo com a solucao de um exercicio do capitulo 2 do livro Thinking Functionally with Haskell (TFwH).
+-- Tarefa, completar este modulo cada aluno contribuindo com a solucao
+-- de um exercicio do capitulo 2 do livro Thinking Functionally with
+-- Haskell (TFwH).
+
